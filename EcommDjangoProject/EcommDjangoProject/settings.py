c8a253ad231de78a28c0dde9af9169e5b63ef9a0
ollie-herridge10 <515238@waes.ac.uk>
2024-04-11 16:57:53 +0100

    created shopping cart

--- EcommDjangoProject/EcommDjangoProject/settings.py
+++ EcommDjangoProject/EcommDjangoProject/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'store', # Django app
+
+    'cart', # Django app
 ]
 
 MIDDLEWARE = [
--- EcommDjangoProject/EcommDjangoProject/urls.py
+++ EcommDjangoProject/EcommDjangoProject/urls.py
@@ -23,9 +23,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     
+    # Admin url
+
     path('admin/', admin.site.urls),
 
+    # Store app
+
     path('', include('store.urls')),
+
+    # Cart app
+
+    path('cart/', include('cart.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
