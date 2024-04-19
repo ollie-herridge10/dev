fc3a1618be550b355e1d6c1b9cbe07f7577a8195
ollie-herridge10 <515238@waes.ac.uk>
2024-04-19 15:14:20 +0100

    update and delete cart function

--- EcommDjangoProject/cart/views.py
+++ EcommDjangoProject/cart/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 
 from .cart import Cart
-
 from store.models import Product
 
 from django.shortcuts import get_object_or_404
-
 from django.http import JsonResponse
 
 
@@ -14,6 +12,7 @@ def cart_summary(request):
     cart = Cart(request)
 
     return render(request, 'cart/cart-summary.html', {'cart':cart})
+
 
 
 def cart_add(request):
@@ -29,18 +28,56 @@ def cart_add(request):
 
         cart.add(product=product, product_qty=product_quantity)
 
+
         cart_quantity = cart.__len__()
+
 
         response = JsonResponse({'qty': cart_quantity})
 
         return response
+        
 
 
 def cart_delete(request):
 
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+
+        cart.delete(product=product_id)
+
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+
+        response = JsonResponse({'qty':cart_quantity, 'total':cart_total})
+
+        return response
+
 
 
 def cart_update(request):
 
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        cart.update(product=product_id, qty=product_quantity)
+
+
+        cart_quantity = cart.__len__()
+
+        cart_total = cart.get_total()
+
+
+        response = JsonResponse({'qty':cart_quantity, 'total':cart_total})
+
+        return response
+
