6b9fb4bc140c5d45977527303d4b44beaa806a38
ollie-herridge10 <515238@waes.ac.uk>
2024-04-22 15:49:27 +0100

    user authentication

--- EcommDjangoProject/EcommDjangoProject/settings.py
+++ EcommDjangoProject/EcommDjangoProject/settings.py
@@ -145,3 +145,8 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email configuration settings:
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
